1abfa885ccb752444647cc43cd81c42916f90ca0
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-10 12:08:40 +0300

    Initial commit - hospital website with React and Tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Navbar(){
+
+
+    return(
+        <header className="bg-blue-200">
+            <img src="" alt="Hospital Logo" />
+            <nav className="bg-white px-6 py-4 flex justify-between items-center">
+                <h1 className="text-xl font-bold text-blue-600">LENNCURE HOSPITAL</h1>
+                <ul className="bg-blue-400 flex space-x-6 text-gray-700 ">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="shop">Shop</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Navbar
