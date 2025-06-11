3095bc1e680b07ac32b705c7d7b682ea8c533ef8
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-11 19:13:41 +0300

    Add the pages and their links

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Shop from "./pages/Shop";
+import Contacts from "./pages/Contacts";
 
 function App() {
-
-
   return (
-    <>
-    <Navbar />
-    <Footer />
-
-    </>
-    
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} /> */}
+            <Route path="/services" element={<Services />} />
+            <Route path="/shop" element={<Shop />} />
+            {/* <Route path="/shop/:id" element={<ProductDetails />} /> */}
+            <Route path="/contact" element={<Contacts />} />
+            {/* <Route path="/checkout" element={<Checkout />} /> */}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
